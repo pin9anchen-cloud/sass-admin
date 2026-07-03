b72e636b777ab2d6ed43e2b6ea7a442c9a4dff97
chenvve <18331911795@163.com>
2026-07-03 18:43:18 +0800

    将登陆参数从URL改为requestBody中获取

--- src/apis/auth.js
+++ src/apis/auth.js
@@ -1,7 +1,7 @@
 // 商家账号相关的请求
 import request from "../utils/request";
 
-// 用 form-urlencoded 请求体传账号密码，避免密码出现在 URL 里
+// 用 JSON 请求体传账号密码，避免密码出现在 URL 里；后端 /auth/login 现在是 @RequestBody
 export function loginAPI(username, password) {
-  return request.post("/auth/login", new URLSearchParams({ username, password }));
+  return request.post("/auth/login", { username, password });
 }
